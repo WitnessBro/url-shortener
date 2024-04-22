6a1fc7a8f063a8f2cbdae2de44aac176425f79fa
wtfbro <wtfbro@yandex-team.ru>
2024-04-22 23:20:37 +0300

    add shorturl_generator.go and shorturl_generator_test.go

--- shortener/shorturl_generator.go
+++ shortener/shorturl_generator.go
@@ -1,1 +1,32 @@
 package shortener
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"github.com/itchyny/base58-go"
+	"math/big"
+	"os"
+)
+
+func sha256Of(input string) []byte {
+	algorithm := sha256.New()
+	algorithm.Write([]byte(input))
+	return algorithm.Sum(nil)
+}
+
+func base58Encoded(bytes []byte) string {
+	encoding := base58.BitcoinEncoding
+	encoded, err := encoding.Encode(bytes)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	return string(encoded)
+}
+
+func GenerateShortUrl(initialUrl string, userId string) string {
+	urlHashBytes := sha256Of(initialUrl + userId)
+	generatedNumber := new(big.Int).SetBytes(urlHashBytes)
+	finalString := base58Encoded([]byte(fmt.Sprintf("%d", generatedNumber)))
+	return finalString[:8]
+}
--- shortener/shorturl_generator_test.go
+++ shortener/shorturl_generator_test.go
@@ -1,1 +1,23 @@
 package shortener
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+const UserId = "e0dba740-fc4b-4977-872c-d360239e6b1a"
+
+func TestShortLinkGenerator(t *testing.T) {
+	initial_link_1 := "https://www.guru3d.com/news-story/spotted-ryzen-threadripper-pro-3995wx-processor-with-8-channel-ddr4,2.html"
+	shortLink_1 := GenerateShortUrl(initial_link_1, UserId)
+
+	initialLink_2 := "https://www.eddywm.com/lets-build-a-url-shortener-in-go-with-redis-part-2-storage-layer/"
+	shortLink_2 := GenerateShortUrl(initialLink_2, UserId)
+
+	initialLink_3 := "https://spectrum.ieee.org/automaton/robotics/home-robots/hello-robots-stretch-mobile-manipulator"
+	shortLink_3 := GenerateShortUrl(initialLink_3, UserId)
+
+	assert.Equal(t, shortLink_1, "AaqZcBo9")
+	assert.Equal(t, shortLink_2, "92bcQLoc")
+	assert.Equal(t, shortLink_3, "9QtiPSVJ")
+}
